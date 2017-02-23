0d12347d0a6b87c67c0eae77745077eb6ca53b58
py4_ <ibtkm2009@gmail.com>
2017-02-23 23:09:26 +0330

    Fix typo in env vars for authentication

--- src/settings/twitter.py
+++ src/settings/twitter.py
@@ -4,6 +4,6 @@ import sys
 import tweepy
 
 
-auth = tweepy.auth.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SERCRET'))
-auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_SECRET'))
+auth = tweepy.auth.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
+auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
 api = tweepy.API(auth)
