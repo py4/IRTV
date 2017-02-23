270c6400988294f0ca778a0893183c3a0746f0de
py4_ <ibtkm2009@gmail.com>
2017-02-24 02:44:58 +0330

    Automatically sleep after rate limit happended

--- src/settings/twitter.py
+++ src/settings/twitter.py
@@ -6,4 +6,4 @@ import tweepy
 
 auth = tweepy.auth.OAuthHandler(os.getenv('CONSUMER_KEY'), os.getenv('CONSUMER_SECRET'))
 auth.set_access_token(os.getenv('ACCESS_TOKEN'), os.getenv('ACCESS_TOKEN_SECRET'))
-api = tweepy.API(auth)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
